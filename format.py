595f40465f55711410209c8760c47c4bd3348669
zxcdanya228 <zxcdanya228@gmail.com>
2022-09-10 15:17:52 +0300

    Add format.py

---
+++ format.py
@@ -0,0 +1,11 @@
+from platform import python_version
+
+
+age = 20
+python_version = 3.8
+
+print(f'Your age is {age}')
+print(f'python_version={python_version}')
+
+print(f'{age = }')
+print(f'{python_version = }')
