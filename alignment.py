45aceebfd755a6161c9a64fdf3948861663d59f4
zxcdanya228 <zxcdanya228@gmail.com>
2022-09-10 15:37:22 +0300

    Add alignment.py

---
+++ alignment.py
@@ -0,0 +1,12 @@
+number = 20
+
+print(f'{number:8}')
+print(f'{number:>8}')
+print(f'{number:<8}')
+print(f'{number:_<8}')
+print(f'{number:_>8}')
+
+x = 20.123
+
+print(f'{x:0<8}')
+print(f'{x:.5f}')
