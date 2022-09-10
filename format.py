afbb4320c6f65d6c41d8b0bfe63de43a94015901
zxcdanya228 <zxcdanya228@gmail.com>
2022-09-10 15:24:07 +0300

    Add to format.py

--- format.py
+++ format.py
@@ -1,11 +1,11 @@
 from platform import python_version
 
-
+# 10
 age = 20
 python_version = 3.8
 
 print(f'Your age is {age}')
 print(f'python_version={python_version}')
 
-print(f'{age = }')
+print(f'{age =}')
 print(f'{python_version = }')
